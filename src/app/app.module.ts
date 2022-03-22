778650c43fce158cb479ef22e77e16e0588d177c
CRISTHIAN URETA <cureta@acity.com.pe>
2022-03-21 20:33:12 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContenidoComponent,
+    ToolbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ContenidoComponent]
 })
 export class AppModule { }
